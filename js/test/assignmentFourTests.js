0b1a043f7c71f0492305f6e6c70e8d1b1b0e4dd8
takushib <38138564+takushib@users.noreply.github.com>
2019-05-30 15:28:06 -0700

    Update assignmentFourTests.js

--- js/test/assignmentFourTests.js
+++ js/test/assignmentFourTests.js
@@ -9,3 +9,28 @@ import java.util.stream.Collectors;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+describe("Score Test", function() {
+  new Game.App();
+  new Game.Engine();
+  it("should be able to compute correct score", function() {
+    var rowsRemoved = Math.random() * (100 - 1) + 1;
+    var correctScore = rowsRemoved * 100;
+
+    expect(Game.Engine._computeScore(rowsRemoved)).toEqual(correctScore);
+  });
+
+});
+
+describe("Shift Test", function() {
+  new Game.App();
+  new Game.Engine();
+  it("should be able to shift piece in both directions", function() {
+    var direction = Math.random() * (1 - (-1)) - 1;
+    var xy = new XY(direction, 0);
+    this._piece.xy = this._piece.xy.plus(xy);
+    if(!this._piece.fits(this.pit)) { this._piece.xy.minus(xy); }
+    expect(Game.Engine.shift(direction)).toEqual(this);
+  });
+});
+    
